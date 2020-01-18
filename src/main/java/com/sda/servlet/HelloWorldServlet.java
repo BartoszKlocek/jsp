8683efc50e3f6eb3fc5bc068d7665f45104c8c1e
bartosz <bartoszklocek1985@gmail.com>
2020-01-18 13:16:05 +0100

    Zadanie 23 ADNOTACJA

--- src/main/java/com/sda/servlet/HelloWorldServlet.java
+++ src/main/java/com/sda/servlet/HelloWorldServlet.java
@@ -10,10 +10,13 @@ import java.io.PrintWriter;
 //Ta adnotaja to alermatywna wers(udunieto plik konfiguracyny z web.xml)
 @WebServlet(name = "HelloWorldServlet", urlPatterns = {"/"})
 public class HelloWorldServlet extends HttpServlet {
+    private String userId, localization;
+
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         PrintWriter writer = response.getWriter();
-        writer.print("Hello World!");
+        writer.println("UserId: " + userId );
+        writer.println("Localization: "+localization);
     }
 }
