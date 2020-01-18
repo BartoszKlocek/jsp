fd33d6bd822c6e8c338aab0b5a3ab5b19e807e02
bartosz <bartoszklocek1985@gmail.com>
2020-01-18 13:04:18 +0100

    Zadanie 21 ALTERNATYWNA WERSJA: usunieto konfigurace z web.xml i wstawiono adnotacje @WebServlet(....) do HelloWorldServlet

--- src/main/java/com/sda/servlet/HelloWorldServlet.java
+++ src/main/java/com/sda/servlet/HelloWorldServlet.java
@@ -1,11 +1,14 @@
 package com.sda.servlet;
 
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+//Ta adnotaja to alermatywna wers(udunieto plik konfiguracyny z web.xml)
+@WebServlet(name = "HelloWorldServlet", urlPatterns = {"/"})
 public class HelloWorldServlet extends HttpServlet {
 
     @Override
